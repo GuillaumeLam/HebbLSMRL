fe957f7ea97394484a03a9cf585d8acb0c7bdcf0
Guillaume <guillaume.lam10100@gmail.com>
2021-08-19 13:14:04 -0400

    Put code into function

--- util/res_analysis.jl
+++ util/res_analysis.jl
@@ -4,17 +4,20 @@ using Plots
 
 include("./data_pipeline.jl")
 
-lsm = model_dict["LSM"](4,2,"cartpole",StableRNG(123))
+function plot_eigen_spec(m)
+      eigen_matrix = eigen(Matrix(m)).vectors
 
-eigen_matrix = eigen(Matrix(lsm.reservoir.layers[2].W[2])).vectors
+      eigen_imag = imag(eigen_matrix)
+      eigen_real = real(eigen_matrix)
 
-eigen_imag = imag(eigen_matrix)
-eigen_real = real(eigen_matrix)
+      function circShape(h,k,r)
+            θ=LinRange(0,2*π, 500)
+            h .+ r*sin.(θ), k .+ r*cos.(θ)
+      end
 
-function circShape(h,k,r)
-      θ=LinRange(0,2*π, 500)
-      h .+ r*sin.(θ), k .+ r*cos.(θ)
+      scatter(vec(x_m),vec(y_m), size=(500,500))
+      plot!(circShape(0,0,1), seriestype=[:shape,], lw = 0.5,c=:green,legend=false,fillalpha=0.2,aspect_ration=1)
 end
 
-scatter(vec(x_m),vec(y_m), size=(500,500))
-plot!(circShape(0,0,1), seriestype=[:shape,], lw = 0.5,c=:green,legend=false,fillalpha=0.2,aspect_ration=1)
+lsm = model_dict["LSM"](4,2,"cartpole",StableRNG(123))
+plot_eigen_spec(lsm.reservoir.layers[2].W[2])
