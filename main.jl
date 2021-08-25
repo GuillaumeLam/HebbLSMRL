008101b532f3a76eb6d43e9faba0fd77d6ea0745
Guillaume <guillaume.lam10100@gmail.com>
2021-08-24 22:47:40 -0400

    Removed seed setting

--- main.jl
+++ main.jl
@@ -6,8 +6,10 @@ include("./util/arg.jl")
 include("./util/data_pipeline.jl")
 
 using DelimitedFiles
+using StableRNGs
 
-seeds = [001993 109603 619089 071198 383163 213556 410290 908818 123123 456456]
+# seeds = [001993 109603 619089 071198 383163 213556 410290 908818 123123 456456]
+seeds = [809669, 689741]
 
 model_type, total_eps = get_main_arg(get_Args())
 
@@ -15,7 +17,7 @@ for (j, total_ep) in enumerate(total_eps)
 	@info "Running each experiments for $total_ep episodes"
 	for (i, seed) in enumerate(seeds)
 		@info "Starting experiment $i"
-		reward = run_exp(seed, model_type; total_eps=total_ep)
+		reward = run_exp(StableRNG(seed), model_type; total_eps=total_ep)
 		@info "Completed $(i/length(seeds)*100)% of experiments of $total_ep episodes"
 		io = open("./results/Q$model_type-total_ep=$total_ep.txt", "a") do io
 			writedlm(io, reward')
