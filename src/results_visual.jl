feb863b110b8b1944aa0e2f03e7bd8ad9e578720
Guillaume Lam <guillaume.lam10100@gmail.com>
2021-12-10 21:48:32 -0500

    Broke up method in pieces

--- src/results_visual.jl
+++ src/results_visual.jl
@@ -27,15 +27,16 @@ function to_str(vec)
         str *= "-"
     end
 
+    # remove trailing dash
     str = chop(str, tail=1)
 
     return str
 end
 
-function get_top(p,v)
-
-    lim = size(v)[2]
+function get_top(p,m)
+    lim = size(m)[2]
     bot = 0
+
     if p >= 100
         bot = 1
     elseif p <= 0
@@ -48,80 +49,104 @@ function get_top(p,v)
     return v[:,bot:end]
 end
 
-function analyze_rewards()
+struct AggrMetric
+    dict::Dict
+
+    function AggrMetric(metrics::AbstractVector)
+        d = Dict()
+        for m in metrics
+            d[m] = Dict()
+        end
+        return new(d)
+    end
+
+    AggrMetric() = AggrMetric(["MEAN", "IQM", "MEDIAN", "BASE"])
+end
+
+function add!(aggr::AggrMetric, metric, ep, val)
+    aggr.dict[metric][ep] = val
+end
+
+
+function analyze_run!(m::AbstractMatrix, aggr=nothing::Union{AggrMetric,Nothing}; top_p=nothing::Union{Int64,Nothing})
+    # order the runs by max val hit, lowest to highest in cols
+    m = sortslices(m, dims=2, by=x->max(x...))
+
+    # get top p% runs (0-100)%
+    if !isnothing(top_p)
+        m = get_top(top_p, m)
+    end
+
+    μ = vec(mean(m, dims=2))
+    σ = vec(std(m, dims=2))
+    x̃ = vec(median(m, dims=2))
+
+    p1 = Plots.plot(
+        [1:length(μ);], μ, ribbon=σ,
+        color=:lightblue,
+        xlabel="Episode",
+        ylabel="Reward",
+        title="Average reward over episode",
+        label=false)
+    p2 = Plots.plot(
+        [1:length(x̃);], x̃, ribbon=σ,
+        color=:lightgreen,
+        xlabel="Episode",
+        ylabel="Reward",
+        title="Median reward over episode",
+        label=false)
+    display(Plots.plot(p1, p2, layout=(2,1)))
+
+    if !isnothing(aggr)
+        tmp_mean = mean(m,dims=1)
+        # MEAN_μ = mean(tmp_mean)
+        # MEAN_σ = std(tmp_mean)
+
+        tmp_iq = mapslices((x)->mean(collect(trim(x,prop=0.25))), m, dims=1)
+        # IQ_μ = mean(tmp_iq)
+        # IQ_σ = std(tmp_iq)
+
+        tmp_med = median(m,dims=1)
+        # MED_μ = mean(tmp_med)
+        # MED_σ = std(tmp_med)
+
+        γ = 200
+        tmp_base = mapslices((x)->mean([min(e,γ) for e in x]), m, dims=1)
+        # base_μ = mean(tmp_base)
+        # base_σ = std(tmp_base)
+
+        n_eps = string(size(m)[1])
+        add!(aggr, "MEAN", n_eps, tmp_mean)
+        add!(aggr, "IQM", n_eps, tmp_iq)
+        add!(aggr, "MEDIAN", n_eps, tmp_med)
+        add!(aggr, "BASE", n_eps, tmp_base)
+    end
+end
+
+function analyze_aggregate(model_type="LSM"::String; top_p=nothing::Union{Int64,Nothing})
     results_path = pwd()*"/results/"
     save_path = pwd()*"/plots/"
 
-    model_type = "LSM"
-
-    aggr = Dict("Mean" => Dict(), "IQM" => Dict(), "MEDIAN" => Dict(), "OG" => Dict())
+    aggr = AggrMetric()
 
     isdir(save_path) || mkdir(save_path)
-
-    eps = Vector{Int64}(undef, 0)
 
     for file in readdir(results_path)
         if !occursin("Q"*model_type, file)
             continue
         end
 
-        v = readdlm(results_path*file)
-
-        # order runs by max val hit, lowest to highest
-        v = sortslices(v, dims=2, by=x->max(x...))
-
-        # get top p% runs (0-100)%
-        p = 50
-
-        v = get_top(p,v)
-
-        n_eps = size(v)[1]
-
-        μ = vec(mean(v, dims=2))
-        σ = vec(std(v, dims=2))
-        x̃ = vec(median(v, dims=2))
-
-        # Plots.plot([1:length(μ);],μ, color=:lightblue, ribbon=σ, label=false)
-        # Plots.plot!([1:length(x̃);],x̃, color=:lightgreen, ribbon=σ, label=false)
-        # Plots.savefig(save_path*"Q$(model_type)_avg&med_e=$(n_eps)")
-
-        display(Plots.plot([1:length(μ);],μ, color=:lightblue, ribbon=σ, label=false))
-        Plots.savefig(save_path*"Q$(model_type)_avg_e=$(n_eps)")
-
-        display(Plots.plot([1:length(x̃);],x̃, color=:lightgreen, ribbon=σ, label=false))
-        Plots.savefig(save_path*"Q$(model_type)_med_e=$(n_eps)")
-
-        tmp_mean = mean(v,dims=1)
-        # MEAN_μ = mean(tmp_mean)
-        # MEAN_σ = std(tmp_mean)
-
-        tmp_iq = mapslices((x)->mean(collect(trim(x,prop=0.25))), v, dims=1)
-        # IQ_μ = mean(tmp_iq)
-        # IQ_σ = std(tmp_iq)
-
-        tmp_med = median(v,dims=1)
-        # MED_μ = mean(tmp_med)
-        # MED_σ = std(tmp_med)
-
-        γ = 200
-        tmp_og = mapslices((x)->mean([min(e,γ) for e in x]), v, dims=1)
-        # OG_μ = mean(tmp_og)
-        # OG_σ = std(tmp_og)
-
-        append!(eps, n_eps)
-        n_eps = string(n_eps)
-
-        aggr["Mean"][n_eps] = tmp_mean
-        aggr["IQM"][n_eps] = tmp_iq
-        aggr["MEDIAN"][n_eps] = tmp_med
-        aggr["OG"][n_eps] = tmp_og
+        m = readdlm(results_path*file)
+        analyze_run!(m, aggr, top_p=top_p)
+        n_eps = size(m)[1]
+        Plots.savefig(save_path*"Q$(model_type)_avg&med_e=$(n_eps)")
     end
 
     colors = distinguishable_colors(4, RGB(0.3,0.3,0.4))
     Plots.plot(layout=(2,2))
 
-    for (i, (plot_title, dict)) in enumerate(aggr)
-        if i == length(aggr)
+    for (i, (plot_title, dict)) in enumerate(aggr.dict)
+        if i == length(aggr.dict)
             display(boxplot!(dict_flatten(dict)..., color=colors[i], label=false, xlabel=plot_title, subplot=i))
         else
             boxplot!(dict_flatten(dict)..., color=colors[i], label=false, xlabel=plot_title, subplot=i)
@@ -130,13 +155,6 @@ function analyze_rewards()
 
     #figure out groupedboxplot
 
+    eps = sort(collect(keys(aggr.dict["IQM"])))
     Plots.savefig(save_path*"Q$(model_type)_[$(to_str(eps))]-eps")
 end
-
-analyze_rewards()
-
-#todo
-# analyze_rewards() -> analyzes rewards in default path
-# analyze_rewards(::String) -> analyzes rewards in given path
-# analyze_rewards(::Matrix) -> analyzes rewards of matrix
-# analyze_rewards(::Vector) -> make matrix of vector and call above
